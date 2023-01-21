cfd8acba4b86220c671929ff296ac1aa06b7f95e
DavidHunterScot <david.hunter@echoverse.cc>
2023-01-21 17:06:50 +0000

    Made some layout changes to About and Contact pages, moved map to separate page and made it full screen.

--- src/index.php
+++ src/index.php
@@ -1,10 +1,14 @@
 <? $current_page = 'about' ?>
 <? include __DIR__ . DIRECTORY_SEPARATOR . '_inc' . DIRECTORY_SEPARATOR . 'header.php' ?>
 
-                <div style="background-image: url('<?= $images_url ?>/IMG_1432-800x493-optimazed.JPG'); background-size: cover; background-repeat: no-repeat; background-position: center; height: 30vw;"></div>
+                <div class="w3-black w3-padding" style="display: flex; flex-direction: column; justify-content: center; align-items: center; color: white; text-shadow: 2px 2px black; background-image: linear-gradient( rgba( 0, 0, 0, 0.3 ), rgba( 0, 0, 0, 0.3 ) ), url('<?= $images_url ?>/IMG_1432-800x493-optimazed.JPG'); background-size: cover; background-repeat: no-repeat; background-position: center; height: 35vw;">
+                    <h2 class="w3-xxxlarge w3-hide-small"><b>Troon Tug Company</b></h2>
+                    <p class="w3-hide-small"><i>Previously Taylor and Taylor, run by Robin Taylor</i></p>
+                    <p class="w3-large w3-hide-small w3-show-medium w3-show-large">has been in the marine trades for over thirty eight years.</p>
+                </div>
                 
                 <div class="w3-content w3-padding-large w3-padding-32 w3-large">
-                    <p>Troon Tug Company, previously Taylor and Taylor, run by Robin Taylor has been in the marine trades for over thirty eight years.</p>
+                    <p class="w3-show-small w3-hide-medium w3-hide-large w3-xlarge">Troon Tug Company, previously Taylor and Taylor, run by Robin Taylor has been in the marine trades for over thirty eight years.</p>
                     <p>Having been involved in fishing and moved into towage, round timber transportation by sea and workboat duties we now concentrate on sea transportation, to remote areas on the West coast of Scotland.</p>
                     <p>We have a range of vessels available from a tug/workboat to a landing craft.</p>
                     <p>Please see the vessel specification pages.</p>
