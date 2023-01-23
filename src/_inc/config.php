10db0db6d9face94a92c68260579fbde2950fd86
DavidHunterScot <david.hunter@echoverse.cc>
2023-01-23 15:05:09 +0000

    Added Poppins WebFont

--- src/_inc/config.php
+++ src/_inc/config.php
@@ -10,5 +10,9 @@ $files_url = "/assets";
 $images_url = $files_url . "/images";
 $downloads_url = $files_url . "/downloads";
 $w3css_url = $files_url . "/w3css";
+$webfonts_url = $files_url . "/webfonts";
+$css_url = $files_url . "/css";
 
 $css_urls[] = $w3css_url . "/4.15/w3.css";
+$css_urls[] = $webfonts_url . "/poppins/poppins.css";
+$css_urls[] = $css_url . "/custom.css";
