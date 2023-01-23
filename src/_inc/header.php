8208a73ab83cb718eeba3a4f1a32fd7b18b4de28
DavidHunterScot <david.hunter@echoverse.cc>
2023-01-23 17:08:34 +0000

    Reverted back to standard include for config file in header

--- src/_inc/header.php
+++ src/_inc/header.php
@@ -1,4 +1,4 @@
-<? include_once __DIR__ . DIRECTORY_SEPARATOR . 'config.php' ?>
+<? include __DIR__ . DIRECTORY_SEPARATOR . 'config.php' ?>
 <? if( ! isset( $current_page ) ) $current_page = ""; ?>
 <!DOCTYPE html>
 <html lang="en">
